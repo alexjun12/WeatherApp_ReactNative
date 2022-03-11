cc25b4c46404ad7ac8c21fdc2040ea905f361c07
alexjun12 <alexjun12@naver.com>
2022-03-11 18:06:21 +0900

    22_03_11

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Vibration, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style = {styles.text}>My Name Is JunYoungCho!!</Text>
+      <Button title="Vib Once" onPress = {() => Vibration.vibrate()} />
       <StatusBar style="auto" />
     </View>
   );
